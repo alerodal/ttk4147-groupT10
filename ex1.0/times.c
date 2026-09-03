c79ea5c91508a29eed9dd46411c6adfd23b9cf64
alerodal <aleksrod@stud.ntnu.no>
2026-09-03 09:47:37 +0100

    test324

--- ex1.0/times.c
+++ ex1.0/times.c
@@ -1,28 +1,36 @@
 #include <string.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <inttypes.h>
+#include <time.h>
 #include <sys/times.h>
 #include <unistd.h>
 
-int main(void) {
-    int histogram[10];
-    memset(histogram, 0, sizeof(histogram));
 
-    struct tms buffer;
+int main(){
 
-    for (int i = 0; i < 10 * 1000 * 1000; i++) {
-        clock_t t1 = times(&buffer);
-        clock_t t2 = times(&buffer);
+        int ns_max = 100;
+        int histogram[ns_max];
+        memset(histogram, 0, sizeof(int)*ns_max);
 
-        clock_t ticks = t2 - t1;
+	struct tms buffer;
+	long tixps = sysconf(_SC_CLK_TCK);
 
-        if (ticks >= 0 && ticks < 10) {
-            histogram[ticks]++;
+	for(int i = 0; i < 10*1000*1000; i++){
+                clock_t t1 = times(&buffer);
+		clock_t t2 = times(&buffer);
+
+                long ns = (t2-t1) * 1000000000L/tixps;
+
+                if(ns >= 0 && ns < ns_max){
+                        histogram[ns]++;
+                }
         }
-    }
 
-    for (int i = 0; i < 10; i++) {
-        printf("%d\n", histogram[i]);
-    }
-
-    return 0;
+        for(int i = 0; i < ns_max; i++){
+                printf("%d\n", histogram[i]);
+        }
+        return 0;
 }
+
+
